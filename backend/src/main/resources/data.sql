aae91b0e619a43a0572d7fdb0625edd01502584b
Luiznt <luiznt@hotmail.com>
2021-07-26 22:46:46 -0300

    Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -22,3 +22,9 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0
  
 INSERT INTO tb_notification (id, text, moment, read, route, user_id) VALUES (1,'Notification text',TIMESTAMP WITH TIME ZONE '2020-11-10T03:00:00Z',FALSE, 'Notification route', 1);
 INSERT INTO tb_notification (id, text, moment, read, route, user_id) VALUES (2,'Notification text',TIMESTAMP WITH TIME ZONE '2020-11-11T03:00:00Z',FALSE, 'Notification route', 2);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML','Trilha principal do curso',1,'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d',1,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum','Tire suas duvidas',2,'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d',2,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives','Lives exclusivas para a turma',3,'https://c.pxhere.com/images/07/b2/f8ed07d6f1a9c740e7703cbb99ee-1443423.jpg!d',0,1);
+
+
